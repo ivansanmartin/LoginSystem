1404124145cdc266f850c5e97370e5e416f11d86
ivansanmartin <ivansanmartin987@gmai.com>
2022-11-20 22:12:15 -0300

    Depured

--- PassEncryp.py
+++
@@ -1,6 +0,0 @@
-import hashlib
-
-# El texto que queremos encriptar lo pasamos a bytes dentro del método md5()
-result = hashlib.md5(b"pass123")
-# al resultado le llamaremos la función hexdigest(), que retorna una encriptación en hexadecimal
-print(result.hexdigest())
